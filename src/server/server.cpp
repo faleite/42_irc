75f4b7764fb2252de19304fbcf1c2718284a6553
Fabricio Leite <fabricio_2310@hotmail.com>
2024-10-16 21:09:35 +0100

    (Update) Handle multiples client

---
+++ src/server/server.cpp
@@ -0,0 +1,151 @@
+#include "Server.hpp"
+
+Server::Server(std::string port, std::string pass): _port(port), _pass(pass)
+{
+	this->_sockfd = -1;
+	createSocket();
+	initServer();
+}
+
+Server::Server() {}
+
+Server::Server(const Server &copyObj)
+{
+	*this = copyObj;
+}
+
+Server &Server::operator=(const Server &assignCopy)
+{
+	if (this != &assignCopy)
+	{
+		this->_port = assignCopy._port;
+		this->_pass = assignCopy._pass;
+		this->_pfds = assignCopy._pfds;
+		this->_sockfd = assignCopy._sockfd;
+	}
+	return *this;
+}
+
+Server::~Server() {}
+
+void Server::createSocket()
+{
+	_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (_sockfd == -1)
+	{
+		close(_sockfd);
+		throw std::runtime_error("Can't create a socket");
+	}
+	int optval = 1;
+	if (setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
+		throw std::runtime_error("Can't set socket options");
+	if (fcntl(_sockfd, F_SETFL, O_NONBLOCK) == -1)
+		throw std::runtime_error("Can't set non_blocking");
+	
+	sockaddr_in servAddr;
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = INADDR_ANY; // Accepts connections on any network interface
+	servAddr.sin_port = htons((std::atoi(_port.c_str())));
+	
+	if (bind(_sockfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) == -1)
+		throw std::runtime_error("Can't be bind to IP/port");
+	if (listen(_sockfd, SOMAXCONN) == -1)
+		throw std::runtime_error("Can't listen");
+}
+
+void Server::acceptClient()
+{
+	sockaddr_in client;
+	socklen_t cliSize = sizeof(client);
+
+	int clifd = accept(_sockfd, (sockaddr *)&client, &cliSize);
+	if (clifd == -1)
+		throw std::runtime_error("Problem with client connecting");
+	
+	pollfd pfd = {clifd, POLLIN, 0};
+	_pfds.push_back(pfd);
+	
+	// FOR NOW::::::::::::FOR NOW
+	// HERE GET HOST NAME FOR NOW
+	char host[NI_MAXHOST];
+	char service[NI_MAXSERV];
+	memset(host, 0, NI_MAXHOST);
+	memset(service, 0, NI_MAXSERV);
+
+	int result = getnameinfo((sockaddr *)&client, cliSize, 
+								host, NI_MAXHOST, service, NI_MAXSERV, 0);
+	if (result != 0)
+		throw std::runtime_error("Can't get hostname");	
+	else
+		std::cout << inet_ntoa(client.sin_addr) << " connected on " << ntohs(client.sin_port) << std::endl;
+}
+
+void Server::initServer()
+{
+	pollfd pfd = {_sockfd, POLLIN, 0};
+	_pfds.push_back(pfd);
+
+	while(true) // siganls
+	{
+		// negative timeout waits forever
+		if (poll(_pfds.data(), _pfds.size(), -1) == -1)
+			throw std::runtime_error("poll error");
+		for (std::vector<pollfd>::iterator it = _pfds.begin(); it != _pfds.end(); it++)
+		{
+			if (it->revents & POLLIN)
+			{
+				if (it->fd == _sockfd)
+				{
+					acceptClient();
+					break ;
+				}
+				std::cout << getMessage(it->fd);
+				if (it->revents & POLLHUP) // Make signal handle 
+				{
+					std::cout << "Client has been disconnected" << std::endl;	
+					break ;
+				}
+			}
+		}
+	}
+}
+
+std::string Server::getMessage(int fd)
+{
+	char buffer[1024]; 
+	std::string message;
+	
+	int bytesRecv = recv(fd, buffer, 1024, 0);
+	if (bytesRecv <= 0)
+		clientExit(fd);
+	else if (bytesRecv > 510) // check in RFC about close connection here
+	{
+		std::cerr << "Message exceeds 512 bytes limit. Closing connection." << std::endl;
+        clientExit(fd);
+	}
+	else
+		message = std::string(buffer, bytesRecv);
+	return (message);
+}
+
+void Server::clientExit(int fd)
+{
+	try
+	{	
+		for (std::vector<pollfd>::iterator it = _pfds.begin(); it != _pfds.end();)
+		{
+			if (it->fd == fd)
+			{
+				it = _pfds.erase(it);
+				close(fd);
+				break ;
+			}
+			else
+				++it;
+		}
+	}
+	catch (const std::exception &e)
+	{
+		throw std::runtime_error("Error in client exit");
+	}
+}
