1d549650ce886bba4a4ab3d5376eb0e96097b3a7
Juan Aldana <juan.martinez43@udea.edu.co>
2024-10-22 09:26:10 +0100

    (update) brodcast message method

--- src/channel/Channel.cpp
+++ src/channel/Channel.cpp
@@ -1,15 +1,16 @@
 #include "Channel.hpp"
 #include <vector>
 
-Channel::Channel(std::string const &name) : _name(name) {
-  std::cout << "The Channel < " << name << " > was created";
+Channel::Channel(std::string const &name)
+    : active(true), _name(name), _topic("") {
+  std::cout << "The Channel < " << name << " > was created" << std::endl;
 }
 
 Channel::~Channel() { std::cout << "Channel Destroyed" << std::endl; }
 
 // GETTERS
-std::string const Channel::getName(void) { return (_name); }
-std::string const Channel::getTopic(void) { return (_topic); }
+std::string const &Channel::getName(void) const { return (_name); }
+std::string const &Channel::getTopic(void) const { return (_topic); }
 
 // SETTER.
 void Channel::setTopic(std::string const &topic) { this->_topic = topic; }
@@ -19,14 +20,13 @@ void Channel::joinChannel(Client *newClient) {
   // Check if is the first client to make ir operator.
   std::vector<Client *>::iterator it;
   it = std::find(channelUsers.begin(), channelUsers.end(), newClient);
-  if (channelUsers.size() == 0) {
-    std::cout << "First client Ever, you are the boss." << std::endl;
-    newClient->setOperator(true);
-    channelUsers.push_back(newClient);
-  } else if (it ==
-             channelUsers
-                 .end()) // can start in 2, as the bot can be create by default
+  if (it ==
+      channelUsers.end()) // can start in 2, as the bot can be create by default
   {
+    if (channelUsers.empty()) {
+      std::cout << "First client Ever, you are the boss." << std::endl;
+      newClient->setOperator(true);
+    }
     channelUsers.push_back(newClient);
     std::cout << " join the Channel " << newClient->getName() << std::endl;
   } else {
@@ -40,5 +40,14 @@ void Channel::leaveChannel(Client *client) {
   if (it != channelUsers.end()) {
     channelUsers.erase(it);
     std::cout << client->getName() << " Has left the Channel" << std::endl;
+  }
+  if (channelUsers.empty())
+    active = false;
+}
+
+void Channel::brodcastMessage(std::string &message) {
+  for (std::vector<Client *>::iterator it = channelUsers.begin();
+       it != channelUsers.end(); ++it) {
+    (*it)->sendMessage(message);
   }
 }
--- src/channel/Channel.hpp
+++ src/channel/Channel.hpp
@@ -4,10 +4,10 @@
 #include "../client/Client.hpp"
 #include <string>
 
-
 class Channel {
 
 private:
+  bool active;
   std::string _name;
   std::string _topic;
   std::vector<Client *> channelUsers;
@@ -16,13 +16,13 @@ public:
   Channel(std::string const &name);
   ~Channel();
   // getters.
-  std::string const getName(void) ;
-  std::string const getTopic(void);
+  std::string const &getName(void) const;
+  std::string const &getTopic(void) const;
   // Setter.
   void setTopic(std::string const &topic);
   // Actions.
-  void joinChannel(Client *newClient) ;
-  void leaveChannel(Client *client) ;
-  void const brodcastMessage(std::string &message) const;
+  void joinChannel(Client *newClient);
+  void leaveChannel(Client *client);
+  void brodcastMessage(std::string &message);
 };
 #endif
