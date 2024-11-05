bab761bf8d0630014ef89dc255b9df47f7063d0d
Juan Aldana <juan.martinez43@udea.edu.co>
2024-11-05 16:06:27 +0000

    (add) add the command msg, list and others

--- src/replies/Replies.cpp
+++ src/replies/Replies.cpp
@@ -1,38 +1,98 @@
 #include "Replies.hpp"
 
-std::string  Replies::ERR_NEEDMOREPARAMS(const std::string &cmd)
-{
+// check for the channel replies
+/*
+       001    RPL_WELCOME
+              "Welcome to the Internet Relay Network
+               <nick>!<user>@<host>"
+       325    RPL_UNIQOPIS
+              "<channel> <nickname>"
+
+       324    RPL_CHANNELMODEIS
+              "<channel> <mode> <mode params>"
+
+       331    RPL_NOTOPIC
+              "<channel> :No topic is set"
+       332    RPL_TOPIC
+              "<channel> :<topic>"
+
+         - When sending a TOPIC message to determine the
+           channel topic, one of two replies is sent.  If
+           the topic is set, RPL_TOPIC is sent back else
+           RPL_NOTOPIC.
+
+       341    RPL_INVITING
+              "<channel> <nick>"
+
+         - Returned by the server to indicate that the
+           attempted INVITE message was successful and is
+           being passed onto the end client.
+
+       342    RPL_SUMMONING
+              "<user> :Summoning user to IRC"
+
+         - Returned by a server answering a SUMMON message to
+           indicate that it is summoning that user.
+
+       346    RPL_INVITELIST
+              "<channel> <invitemask>"
+       347    RPL_ENDOFINVITELIST
+              "<channel> :End of channel invite list"
+
+         - When listing the 'invitations masks' for a given channel,
+           a server is required to send the list back using the
+           RPL_INVITELIST and RPL_ENDOFINVITELIST messages.  A
+           separate RPL_INVITELIST is sent for each active mask.
+           After the masks have been listed (or if none present) a
+           RPL_ENDOFINVITELIST MUST be sent.
+
+       348    RPL_EXCEPTLIST
+              "<channel> <exceptionmask>"
+       349    RPL_ENDOFEXCEPTLIST
+              "<channel> :End of channel exception list"
+
+
+        353   RPL_NAMREPLY
+              "( "=" / "*" / "@" ) <channel>
+               :[ "@" / "+" ] <nick> *( " " [ "@" / "+" ] <nick> )
+         - "@" is used for secret channels, "*" for private
+           channels, and "=" for others (public channels).
+              */
+
+std::string Replies::ERR_NEEDMOREPARAMS(const std::string &cmd) {
   return (":jf.irc 461 * " + cmd + " :Not enough parameters");
 }
 
-std::string  Replies::ERR_UNKNOWNCOMMAND(const std::string &cmd)
-{
+std::string Replies::ERR_UNKNOWNCOMMAND(const std::string &cmd) {
   return (":jf.irc 421 * " + cmd + " :Unknown command");
 }
 
-std::string  Replies::ERR_PASSWDMISMATCH()
-{
-  return(":jf.irc 464 * :Password incorrect");
+std::string Replies::ERR_PASSWDMISMATCH() {
+  return (":jf.irc 464 * :Password incorrect");
 }
 
-std::string  Replies::ERR_NOTREGISTERED()
-{
-  return(":jf.irc 451 * :You need to register before you can use that command"); // ":You have not registered"
+std::string Replies::ERR_NOTREGISTERED() {
+  return (":jf.irc 451 * :You need to register before you can use that "
+          "command"); // ":You have not registered"
 }
 
-std::string  Replies::ERR_ALREADYREGISTRED()
-{
-  return(":jf.irc 462 * :You may not reregisterd");
+std::string Replies::ERR_ALREADYREGISTRED() {
+  return (":jf.irc 462 * :You may not reregisterd");
 }
 
-std::string  Replies::ERR_NICKNAMEINUSE(const std::string &nick)
-{
-  return(":jf.irc 433 * " + nick + " :Nickname is already in use");
+std::string Replies::ERR_NICKNAMEINUSE(const std::string &nick) {
+  return (":jf.irc 433 * " + nick + " :Nickname is already in use");
 }
 
-std::string  Replies::RPL_NAMREPLY(void)
-{
-  // :ergo.test 353 juan = #local :@juan
-  return(":jf.irc 353 juan = #local :@juan");
-  //  + channel + " "  + nick + ":Nickname is already in use");
+// 001.
+std::string Replies::RPL_WELCOME(const std::string &nick,
+                                 const std::string &user,
+                                 const std::string &channel_name) {
+  // Opcion 1. 
+  return std::string(":") + nick + "!" + user + "@localhoost" + " JOIN " +
+         channel_name + " * :" + user + "\r\n";
+
+  // Opcion 2.
+  // return std::string(":jf.irc 433") + nick + "!" + user + "@localhoost" +
+  //        " JOIN " + channel_name + " * :" + user + "\r\n";
 }
--- src/replies/Replies.hpp
+++ src/replies/Replies.hpp
@@ -1,19 +1,22 @@
 #ifndef REPLIES_HPP
 #define REPLIES_HPP
 
-#include "../server/Server.hpp"
 #include "../channel/Channel.hpp"
+#include "../server/Server.hpp"
+#include <string>
 
-class Replies
-{
-	public:
-		static std::string  ERR_PASSWDMISMATCH();
-		static std::string  ERR_NOTREGISTERED();
-		static std::string  ERR_ALREADYREGISTRED();
-		static std::string  ERR_NEEDMOREPARAMS(const std::string &cmd);
-		static std::string  ERR_UNKNOWNCOMMAND(const std::string &cmd);
-		static std::string  ERR_NICKNAMEINUSE(const std::string &nick);
-		static std::string  RPL_NAMREPLY(void);
+class Replies {
+public:
+  static std::string ERR_PASSWDMISMATCH();
+  static std::string ERR_NOTREGISTERED();
+  static std::string ERR_ALREADYREGISTRED();
+  static std::string ERR_NEEDMOREPARAMS(const std::string &cmd);
+  static std::string ERR_UNKNOWNCOMMAND(const std::string &cmd);
+  static std::string ERR_NICKNAMEINUSE(const std::string &nick);
+  static std::string RPL_NAMREPLY(void);
+  static std::string RPL_WELCOME(const std::string &nick,
+                                 const std::string &user,
+                                 const std::string &channel_name);
 };
 
 #endif // REPLIES_HPP
