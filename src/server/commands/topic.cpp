498a0a71719ae0ffcfe9810aadafde9cea8438b4
Juan Aldana <juan.martinez43@udea.edu.co>
2024-11-14 21:35:55 +0000

    (update) the topics of the channel

--- src/server/commands/topic.cpp
+++ src/server/commands/topic.cpp
@@ -8,43 +8,35 @@
 // What we handle.
 
 void Server::topic(Client &client, const std::string &cmd,
-                   const std::vector<std::string> &param)
-{
+                   const std::vector<std::string> &param) {
   (void)cmd;
-  if (param.size() == 1)
-  {
+  if (param.size() == 1) {
     if (findChannel(param[0]) &&
-        _channels[param[0]].isOnChannel(client.getNickName()))
-    {
-      if (_channels[param[0]].getTopic().empty())
-      {
+        _channels[param[0]].isOnChannel(client.getNickName())) {
+      if (_channels[param[0]].getTopic().empty()) {
         client.getMessage(Replies::RPL_NOTOPIC(param[0]));
         return;
       }
-      if (_channels[param[0]].getRestrictedTopic())
-      {
+      if (_channels[param[0]].getRestrictedTopic()) {
         client.getMessage(":The topic of this channel is restricted");
-        // _channels[param[0]].broadcastMessage("The topic of this channel is restricted", &client);
         return;
-      }
-      else
-      {
-        client.getMessage(Replies::RPL_TOPIC(param[0], _channels[param[0]].getTopic()));
+      } else {
+        client.getMessage(
+            Replies::RPL_TOPIC(param[0], _channels[param[0]].getTopic()));
         return;
       }
     }
-  }
-  else if (param.size() < 2 || param[1].empty())
+  } else if (param.size() < 2 || param[1].empty()) {
+    client.getMessage(Replies::ERR_NEEDMOREPARAMS(cmd));
     return;
-  if (!client.getIsOperator())
-  {
-    client.getMessage("Only Operators can change the channel mode");
+  }
+  if (!client.getIsOperator()) {
+    client.getMessage(Replies::ERR_CHANOPRIVSNEEDED(param[0]));
     return;
   }
   std::string mess;
   std::ostringstream oss;
-  for (size_t i = 1; i < param.size(); ++i)
-  {
+  for (size_t i = 1; i < param.size(); ++i) {
     if (i != 0)
       oss << " ";
     oss << param[i];
